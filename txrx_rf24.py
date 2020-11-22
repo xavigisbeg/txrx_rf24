aafaef93e50f0a4d482cefa920e23d3308e1763a
nader.clement <nader.clement@gmail.com>
2020-11-22 13:43:30 +0100

    Localized all the functions Created a main function Set prefixes to variables:   p_ -> for parameters of functions   r_ -> for returns of functions   pr_ -> for parameters/returns of functions   E_ -> for enumeration type globals   UPPER_CASE -> for constant globals

--- txrx_rf24.py
+++ txrx_rf24.py
@@ -1,186 +1,177 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import time
-import os
+# import os
 from RF24 import *
 import RPi.GPIO as GPIO
-import math
+# import math
 import zlib
 
-''' Functions definition '''
+# ----------- Pin set-up ----------- #
+
+# Set the CE and CSN pins
+RADIO = RF24(22, 0)
+
+# Set the IRQ pin. Disconnected by the moment (GPIO24)
+IRQ_GPIO_PIN = None
+# IRQ_GPIO_PIN = 24
+
+# ----------- Constants definitions ----------- #
+
+PIPES = [0xF0F0F0F0E1, 0xF0F0F0F0D2]
+
+# Role selection (0 --> receiver / 1 --> sender)
+ROLE = 0
+
+# States definition
+E_INI_STATE = 0
+E_SENDER_SEND_STATE = 1
+E_SENDER_RECEIVE_STATE = 2
+E_RECEIVER_STATE = 3
 
 
-def ini_state_function(l_role):
+# ----------- Functions definition ----------- #
 
-    radio.begin()
-    radio.enableDynamicPayloads()  # Dynamic ACK enables
-    radio.setRetries(5, 15)  # radio.setchannel()
+def ini_state_function():
+    RADIO.begin()
+    RADIO.enableDynamicPayloads()  # Dynamic ACK enables
+    RADIO.setRetries(5, 15)  # RADIO.setchannel()
 
-    if l_role == 0:  # Receiver
-        if irq_gpio_pin is not None:
+    if ROLE == 0:  # Receiver
+        if IRQ_GPIO_PIN is not None:
             # set up callback for irq pin
             GPIO.setmode(GPIO.BCM)
-            GPIO.setup(irq_gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.add_event_detect(irq_gpio_pin, GPIO.FALLING, callback=try_read_data)  # TODO
-        radio.openWritingPipe(pipes[1])
-        radio.openReadingPipe(1, pipes[0])
-        radio.startListening()
-        l_state = RECEIVER_STATE
+            GPIO.setup(IRQ_GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(IRQ_GPIO_PIN, GPIO.FALLING, callback=try_read_data)  # TODO
+        RADIO.openWritingPipe(PIPES[1])
+        RADIO.openReadingPipe(1, PIPES[0])
+        RADIO.startListening()
+        r_state = E_RECEIVER_STATE
 
     else:  # Sender
-        radio.openWritingPipe(pipes[0])
-        radio.openReadingPipe(1, pipes[1])
-        l_state = SENDER_SEND_STATE
-    return l_state
+        RADIO.openWritingPipe(PIPES[0])
+        RADIO.openReadingPipe(1, PIPES[1])
+        r_state = E_SENDER_SEND_STATE
+    return r_state
 
 
-def sender_send_function():
-    global state
-    global still_messages_to_send
-    global message_numeration
-    global file_content
-    global last_character_position
-
-    radio.stopListening()
-    content_to_send = b""
-    print(len(file_content))
-    if len(file_content) > 1:
-        content_to_send = file_content
+def sender_send_function(p_file_content, pr_still_messages_to_send):
+    RADIO.stopListening()
+    # content_to_send = b""
+    print(len(p_file_content))
+    if len(p_file_content) > 1:
+        content_to_send = p_file_content
     else:
         # EOF
         content_to_send = b"0"
-        still_messages_to_send = 0
-    radio.write(content_to_send[0:31])
-    text_to_send = bytes()
-    state = SENDER_RECEIVE_STATE
+        pr_still_messages_to_send = 0
+    RADIO.write(content_to_send[0:31])
+    # text_to_send = bytes()
+    r_state = E_SENDER_RECEIVE_STATE
+    return r_state, pr_still_messages_to_send
 
 
-def sender_receive_function():
-    global state
-    global still_messages_to_send
-    global message_numeration
-    global communication_on
-    global file_content
-    global last_character_position
-
-    radio.startListening()
+def sender_receive_function(pr_file_content, p_still_messages_to_send):
+    RADIO.startListening()
 
     # Wait here until we get a response, or timeout
     started_waiting_at = millis()
     timeout = False
-    while (not radio.available()) and (not timeout):
+    while (not RADIO.available()) and (not timeout):
         if (millis() - started_waiting_at) > 500:
             timeout = True
 
     if timeout:
-        print('failed, response timed out.')
+        print("failed, response timed out.")
     else:  # Message received
-        lent = radio.getDynamicPayloadSize()
-        receive_payload = radio.read(lent)
-        response_received = bytes(receive_payload)
+        lent = RADIO.getDynamicPayloadSize()
+        receive_payload = RADIO.read(lent)
+        # response_received = bytes(receive_payload)
         # Check if it is the last message
         if len(receive_payload) == 1:
             pass
             # Check if the character of the EOF is correct
             # communication_on = 0
             # Prepare next message to be send
-        if still_messages_to_send == 1:
-            file_content = file_content.replace(file_content[0:31], b"")
+        if p_still_messages_to_send == 1:
+            pr_file_content = pr_file_content.replace(pr_file_content[0:31], b"")
     time.sleep(0.1)
-    state = SENDER_SEND_STATE
+    r_state = E_SENDER_SEND_STATE
+    return r_state, pr_file_content
 
 
-def receiver_function():
-    global state
-    global message_numeration_receiver
-    global message_to_add_to_file
-    global communication_on
-    global compressed_file_received
-
-    if radio.available():
-        lent = radio.getDynamicPayloadSize()
-        receive_payload = radio.read(lent)
+def receiver_function(pr_message_to_add_to_file, pr_communication_on, pr_compressed_file_received):
+    if RADIO.available():
+        lent = RADIO.getDynamicPayloadSize()
+        receive_payload = RADIO.read(lent)
         if len(receive_payload) == 1:  # Last message
             print("Last message")
-            compressed_file_received += message_to_add_to_file
-            print(len(compressed_file_received))
-            decompressed_bytes = zlib.decompress(compressed_file_received, wbits=15)
-            with open('textfile_r.txt', "wb") as text_file:
+            pr_compressed_file_received += pr_message_to_add_to_file
+            print(len(pr_compressed_file_received))
+            decompressed_bytes = zlib.decompress(pr_compressed_file_received, wbits=15)
+            with open("textfile_r.txt", "wb") as text_file:
                 text_file.write(decompressed_bytes)
-            communication_on = 0
+            pr_communication_on = 0
         else:  # Message expected
-            if message_to_add_to_file != receive_payload:
-                compressed_file_received += message_to_add_to_file
-            message_to_add_to_file = receive_payload
+            if pr_message_to_add_to_file != receive_payload:
+                pr_compressed_file_received += pr_message_to_add_to_file
+            pr_message_to_add_to_file = receive_payload
 
-        radio.stopListening()
+        RADIO.stopListening()
         # Send the ACK (in this case the same message)(future implementation)
-        radio.write(receive_payload)
-        radio.startListening()
+        RADIO.write(receive_payload)
+        RADIO.startListening()
+        return pr_message_to_add_to_file, pr_communication_on, pr_compressed_file_received
 
 
-''' Pin set-up '''
-
-# Set the CE and CSN pins
-radio = RF24(22, 0)
-
-# Set the IRQ pin. Disconnected by the moment (GPIO24)
-irq_gpio_pin = None
-# irq_gpio_pin = 24
-
-''' Constants definitions '''
-
-pipes = [0xF0F0F0F0E1, 0xF0F0F0F0D2]
-
-# Role selection (0 --> receiver / 1 --> sender)
-role = 0
-
-# States definition
-INI_STATE = 0
-SENDER_SEND_STATE = 1
-SENDER_RECEIVE_STATE = 2
-RECEIVER_STATE = 3
-state = INI_STATE
-
-# Variables
-communication_on = 1  # ON --> 1 / 0 --> OFF
-message_numeration = 1  # Messages numeration --> 1-9 / 0 last message
-message_numeration_receiver = 1
-message_to_add_to_file = b""  # Message to write into text file
-last_character_position = 0
-still_messages_to_send = 1
-compressed_file_received = b""
-
-''' Read & write file text '''
-
-# Read text file and transform it into bytes
-with open('textfile.txt', "rb") as f:
-    file = f.read()
-file_content = zlib.compress(file, level=9)
+def millis():
+    return int(round(time.time() * 1000))
 
 
-# Open the text file were the data will be printed into
-with open('textfile_r.txt', "wb") as text_file:
-    text_file.write(b"")
+def main():
+    # Variables
+    state = E_INI_STATE
+    communication_on = 1  # ON --> 1 / 0 --> OFF
+    # message_numeration = 1  # Messages numeration --> 1-9 / 0 last message
+    # message_numeration_receiver = 1
+    message_to_add_to_file = b""  # Message to write into text file
+    # last_character_position = 0
+    still_messages_to_send = 1
+    compressed_file_received = b""
 
-''' Start comunnication '''
+    # ----------- Read & write file text ----------- #
 
-# Time start
-millis = lambda: int(round(time.time() * 1000))
+    # Read text file and transform it into bytes
+    with open("textfile.txt", "rb") as f:
+        file = f.read()
+    file_content = zlib.compress(file, level=9)
 
-print('Start')
+    # Open the text file were the data will be printed into
+    with open("textfile_r.txt", "wb") as text_file:
+        text_file.write(b"")
 
-while communication_on == 1:
-    if state == INI_STATE:
-        state = ini_state_function(role)
-    elif state == SENDER_SEND_STATE:
-        sender_send_function()
-    elif state == SENDER_RECEIVE_STATE:
-        sender_receive_function()
-    elif state == RECEIVER_STATE:
-        receiver_function()
-    else:
-        print("Error: No state selected")
+    # ----------- Start communication ----------- #
 
-radio.stopListening()
-print('Finish')
+    # Time start
+    print("Start")
+
+    while communication_on == 1:
+        if state == E_INI_STATE:
+            state = ini_state_function()
+        elif state == E_SENDER_SEND_STATE:
+            state, still_messages_to_send = sender_send_function(file_content, still_messages_to_send)
+        elif state == E_SENDER_RECEIVE_STATE:
+            state, file_content = sender_receive_function(file_content, still_messages_to_send)
+        elif state == E_RECEIVER_STATE:
+            message_to_add_to_file, communication_on, compressed_file_received = \
+                receiver_function(message_to_add_to_file, communication_on, compressed_file_received)
+        else:
+            print("Error: No state selected")
+
+    RADIO.stopListening()
+    print("Finish")
+
+
+if __name__ == "__main__":
+    main()
